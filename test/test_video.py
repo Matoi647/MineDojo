b9b37ca3e83cf5444a657a2438cc33a73ea5a0ae
Matoi647 <1524186933@qq.com>
2024-03-20 15:32:41 +0800

    fix ForgeGradle issue

--- test/test_video.py
+++ test/test_video.py
@@ -2,13 +2,15 @@ import minedojo
 import cv2
 import numpy as np
 
+height, width = 480, 768
+
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-output_video = cv2.VideoWriter('survival_sword_food.mp4', fourcc, 20.0, (256, 160))
+output_video = cv2.VideoWriter('survival_sword_food.mp4', fourcc, 20.0, (width, height))
 
 env = minedojo.make(
     task_id="survival_sword_food",
     # image_size=(160, 256)
-    image_size=(640, 1024)
+    image_size=(height, width)
 )
 # env = minedojo.make(
 #     task_id="harvest_wool_with_shears_and_sheep",
@@ -19,10 +21,18 @@ for i in range(1000):
     act = env.action_space.no_op()
     act[0] = 1    # forward/backward
     if i % 10 == 0:
-        act[2] = 1    # jump
-        act[4] += 1
-    if i % 8 == 0:
-        act[4] -= 1
+        act[2] = 1      # jump
+    if i % 5 == 0:
+        if np.random.rand() > 0.5:
+            act[4] += 1
+        else:
+            act[4] -= 1
+    # if i % 10 == 2:
+    #     act[4] += 1     # look right
+    # if i % 10 == 6:     
+    #     act[4] -= 1     # look left
+
+
     obs, reward, done, info = env.step(act)
 
     print(i)
